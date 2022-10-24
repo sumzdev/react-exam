282faaab74f111fc01b619dc5b04783adb120338
sunhwa park <sumz.ko@gmail.com>
2022-10-24 18:04:35 +0900

    React로 사고하기 - 3,4단계 : State 정하기

--- src/ThinkReact/FilterableProductTable.js
+++ src/ThinkReact/FilterableProductTable.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import ProductTable from "./ProductTable";
 import SearchBar from "./SearchBar";
 
 export default function FilterableProductTable({ products }) {
+  let [search, setSearch] = useState("");
+  let [onlyStock, setOnlyStock] = useState(false);
+
+  const onClickCheckbox = () => {
+    setOnlyStock(!onlyStock);
+  };
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+        onlyStock={onlyStock}
+        onClickCheckbox={onClickCheckbox}
+        onChangeSearch={onChangeSearch}
+      />
+      <ProductTable products={products} search={search} onlyStock={onlyStock} />
     </>
   );
 }
--- src/ThinkReact/SearchBar.js
+++ src/ThinkReact/SearchBar.js
@@ -1,9 +1,13 @@
-export default function SearchBar() {
+export default function SearchBar({
+  onlyStock,
+  onlyClickCheckbox,
+  onChangeSearch,
+}) {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." onChange={onChangeSearch} />
       <div>
-        <input type="checkbox" />
+        <input type="checkbox" value={onlyStock} onClick={onlyClickCheckbox} />
         {" Only show products in stock"}
       </div>
     </form>
